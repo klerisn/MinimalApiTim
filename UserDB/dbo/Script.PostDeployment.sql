86badd89cbad1d026ef881ed521253895d9ddd34
Klerison <klerisondesign@gmail.com>
2023-06-29 18:18:13 -0400

    Adicionar arquivos de projeto.

---
+++ UserDB/dbo/Script.PostDeployment.sql
@@ -0,0 +1,8 @@
+﻿if not exists (select 1 from dbo[User])
+begin
+	insert into dbo.[User] (FirstName, LastName)
+	values ('Klerison', 'Monteiro'),
+	('Kleri', 'Monti'),
+	('Kleris', 'Montis'),
+	('Klerin', 'Montin');
+end
