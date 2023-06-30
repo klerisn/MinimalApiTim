dbc99562d188482ae443b4600a07d582de86d201
Klerison <klerisondesign@gmail.com>
2023-06-30 16:51:19 -0400

    added database stuff

---
+++ DataAccess/Data/UserData.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.DbAccess;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data;
+public class UserData
+{
+    private readonly ISqlDataAccess _db;
+    public UserData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+    public Task<IEnumerable<UserModel>> GetUsers() =>
+        _db.LoadData<UserModel, dynamic>("dbo.spUser_GetAll", new { });
+}
