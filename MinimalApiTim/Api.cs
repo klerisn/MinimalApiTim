72564e952157f7130655543dbae87c7690ff8b39
Klerison <Klerison@DESKTOP-043B7P4>
2023-07-02 23:31:49 -0400

    added GetUsers method at Api Class

--- MinimalApiTim/Api.cs
+++ MinimalApiTim/Api.cs
@@ -7,7 +7,19 @@ public static class Api
 
     public static void ConfigureApi(this WebApplication app)
     {
-
+        // All of my Api endpoint mapping
+        app.MapGet("/Users", GetUsers);
     }
 
+    private static async Task<IResult> GetUsers(IUserData data)
+    {
+        try
+        {
+            return Results.Ok(data.GetUsers());
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
 }
